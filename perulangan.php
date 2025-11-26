ebb1b4888d9410e2471084af66749319186c85e5
Rizkulloh <rizkullahalfriansyah92@gmail.com>
2025-11-26 23:34:26 +0700

    Update

---
+++ perulangan.php
@@ -0,0 +1,24 @@
+<?php
+// Perulangan
+// for
+// while
+// do..while
+// foreach : perulangan khusus array
+// ============================================================
+
+// for
+for ($i = 0; $i < 5; $i++) {
+    echo "ini perulangan for <br>";
+}
+
+// while
+$i = 0;
+while ($i < 5) {
+    echo "ini while<br>";
+    $i++;
+}
+
+// do while
+do {
+    echo "ini do while <br>";
+} while ($i < 5);
