96d69fe830788cbf6e95d40f87687fccba1c28ff
Rizkulloh Alpriyansah <rizkullahalfriansyah92@gmail.com>
2025-11-27 09:31:31 +0700

    Update percabangan.php

--- percabangan.php
+++ percabangan.php
@@ -13,3 +13,27 @@ if ($x < 20) {
 } else {
     echo "salah";
 }
+
+
+// Ternary operator
+$x = 20;
+echo ($x < 20) ? "Benar" : (($x == 20) ? "Bingo!" : "Salah");
+
+
+// switch
+$x = 20;
+
+switch ($x) {
+    case 10:
+        echo "Nilai x adalah 10";
+        break;
+    case 15:
+        echo "Nilai x adalah 15";
+        break;
+    case 20:
+        echo "Bingo!";
+        break;
+    default:
+        echo "Nilai x tidak dikenal";
+}
+
